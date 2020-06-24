652c596d7b542b0834ef740cc979f546982f5f63
Stuart McClintock <Stuart.e.mcc@gmail.com>
2020-06-23 21:16:46 -0700

    identify coordinate of face upon touch

--- Whack-A-Trump/GameScene.swift
+++ Whack-A-Trump/GameScene.swift
@@ -42,17 +42,34 @@ class GameScene: SKScene {
         
         for row in 0..<5 {
             for col in 0..<6 {
-                addFace(at: CGPoint(x:70+Int(gapX*CGFloat(row)), y:Int(frame.midY)-Int(CGFloat(col)*gapY)+30))
+                addFace(at: CGPoint(x:70+Int(gapX*CGFloat(row)), y:Int(frame.midY)-Int(CGFloat(col)*gapY)+30), name:String(col)+","+String(row))
             }
         }
     }
     
-    func addFace(at position: CGPoint){
+    func addFace(at position: CGPoint, name:String){
         let newFace = SKSpriteNode(imageNamed: "trump")
         newFace.position = position
         newFace.zPosition = 1
         newFace.alpha = 1
+        newFace.name = name
         addChild(newFace)
         faces.append(newFace)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch:UITouch = touches[touches.startIndex] as UITouch
+        let positionInScene = touch.location(in: self)
+        let touchedNode = self.nodes(at: positionInScene)
+        
+        print("******")
+        let description = touchedNode.description
+        let name = description.components(separatedBy: " ")[1]
+        let coords = name.components(separatedBy: "'")[1]
+        let splitCoords = coords.components(separatedBy: ",")
+        let xCoord = splitCoords[0]
+        let yCoord = splitCoords[1]
+        print(xCoord, yCoord)
+        print("******")
     }
 }
