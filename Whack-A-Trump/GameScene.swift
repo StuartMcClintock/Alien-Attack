f0f7bcfb089cbf11014609879acb71c19413f501
Stuart McClintock <Stuart.e.mcc@gmail.com>
2020-07-06 12:02:10 -0700

    finish ability of SelectModeScene to set isBlitz in app delegate

--- Whack-A-Trump/GameScene.swift
+++ Whack-A-Trump/GameScene.swift
@@ -41,6 +41,7 @@ class GameScene: SKScene {
         let app = UIApplication.shared
         del = app.delegate as? AppDelegate
         
+        print(del.isBlitz)
         
         let background = SKSpriteNode(imageNamed: "whitehouse")
         background.position = CGPoint(x:frame.midX, y:frame.midY)
--- Whack-A-Trump/SelectModeScene.swift
+++ Whack-A-Trump/SelectModeScene.swift
@@ -10,8 +10,12 @@ import Foundation
 import SpriteKit
 
 class SelectModeScene: SKScene{
+    var del: AppDelegate!
     
     override func didMove(to view: SKView) {
+        let app = UIApplication.shared
+        del = app.delegate as? AppDelegate
+        
         self.backgroundColor = SKColor.init(displayP3Red: 128, green: 235, blue: 255, alpha: 1)
         let headerLabel = SKLabelNode(fontNamed: "Damascus")
         headerLabel.fontSize = 94.0
@@ -31,30 +35,41 @@ class SelectModeScene: SKScene{
         
         let standardButton = SKSpriteNode(color: standardButtonColor, size: buttonSize)
         standardButton.position = standardPointPosition
+        standardButton.name = "standard"
         addChild(standardButton)
         let standardText = SKLabelNode(text: "Standard")
         standardText.position = CGPoint(x:standardPointPosition.x, y:standardPointPosition.y-25)
         standardText.fontName = "DIN Alternate Bold"
         standardText.fontColor = SKColor.white
         standardText.fontSize = 74
+        standardText.name = "standard"
         addChild(standardText)
         
         let blitzButton = SKSpriteNode(color: blitzButtonColor, size: buttonSize)
         blitzButton.position = blitzPointPosition
+        blitzButton.name = "blitz"
         addChild(blitzButton)
         let blitzText = SKLabelNode(text: "BLITZ ! !")
         blitzText.position = CGPoint(x:blitzPointPosition.x, y:blitzPointPosition.y-25)
         blitzText.fontName = "DIN Alternate Bold"
         blitzText.fontColor = SKColor.white
         blitzText.fontSize = 74
+        blitzText.name = "blitz"
         addChild(blitzText)
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         for touch in touches{
-            print(touch.location(in: self))
+            let touchedNode = atPoint(touch.location(in: self))
+            if touchedNode.name == "blitz"{
+                del.isBlitz = true
+                startGame()
+            }
+            if touchedNode.name == "standard"{
+                del.isBlitz = false
+                startGame()
+            }
         }
-        startGame()
     }
     
     func startGame(){
