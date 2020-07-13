eb686246cb4e2bf2116b26842ff092721ee5a36f
Stuart McClintock <Stuart.e.mcc@gmail.com>
2020-07-12 17:45:29 -0700

    satisfied with animation of element removal

---
+++ Whack-A-Trump/AwardDisplayScene.swift
@@ -0,0 +1,16 @@
+//
+//  AwardDisplayScene.swift
+//  Whack-A-Trump
+//
+//  Created by Stuart McClintock on 7/12/20.
+//  Copyright © 2020 Stuart McClintock. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class AwardDisplayScene: SKScene{
+    override func didMove(to view: SKView) {
+        //pass
+    }
+}
